c04582419053aa6e7f99a957f9628a3b04081cc2
Lexxkit <lexxkit@gmail.com>
2022-12-19 11:32:06 +0000

    Initial commit

---
+++ src/main/java/com/lexxkit/App.java
@@ -0,0 +1,13 @@
+package com.lexxkit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
